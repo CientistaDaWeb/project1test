b997d6f0bd95d9eb9f95a4044eca643874095037
Fernando Henrique <fernando@webscientist.com.br>
2024-09-05 19:30:41 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
 <?php
 
-echo 'You are in '. $_SERVER['HTTP_HOST']. '<br /><br /> :D Welcome to the magic land!';
+echo '<h1>You are in '. $_SERVER['HTTP_HOST']. '<br /><br /> :D Welcome to the magic land!</h1';
 
